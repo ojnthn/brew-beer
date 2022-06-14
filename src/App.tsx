a03064c9e8243092ba430e21155e22f98d7fa56c
Jonathan <jonathan.reichel@w2o.com.br>
2022-06-14 12:30:56 -0300

    💄 Melhorias visuais.

--- src/App.tsx
+++ src/App.tsx
@@ -12,49 +12,67 @@ import Footer from "./ui/components/Footer.component";
 
 const content = [
   {
-    name: "Cerveja",
+    name: "Cerveja Pilsner",
     value: 12.56,
     image: beer,
+    description:
+      "A Pale Lager original, desenvolvida como receita da cerveja Pilsner Urquell. São caracterizadas por um lúpulo acentuado no aroma e sabor. Também chamadas de Pilsener e Pilsen. Podem aparecer em 2 estilos um pouco diferentes, principalmente devido à escola: Bohemian/Czech Pilsner , representada pela Pilsner Urquell e Budweiser Budvar/Czechvar, ambas Tchecas e meio difíceis de encontrar; German Pilsner, facilmente encontradas nas representantes Bitburger, Warsteiner, Konig Pilsener, Spaten Pils.",
   },
   {
     name: "Cerveja IPA",
     value: 12.56,
     image: beer,
+    description:
+      "A Pale Lager original, desenvolvida como receita da cerveja Pilsner Urquell. São caracterizadas por um lúpulo acentuado no aroma e sabor. Também chamadas de Pilsener e Pilsen. Podem aparecer em 2 estilos um pouco diferentes, principalmente devido à escola: Bohemian/Czech Pilsner , representada pela Pilsner Urquell e Budweiser Budvar/Czechvar, ambas Tchecas e meio difíceis de encontrar; German Pilsner, facilmente encontradas nas representantes Bitburger, Warsteiner, Konig Pilsener, Spaten Pils.",
   },
   {
     name: "Cerveja Pilsen",
     value: 12.56,
     image: beer,
+    description:
+      "A Pale Lager original, desenvolvida como receita da cerveja Pilsner Urquell. São caracterizadas por um lúpulo acentuado no aroma e sabor. Também chamadas de Pilsener e Pilsen. Podem aparecer em 2 estilos um pouco diferentes, principalmente devido à escola: Bohemian/Czech Pilsner , representada pela Pilsner Urquell e Budweiser Budvar/Czechvar, ambas Tchecas e meio difíceis de encontrar; German Pilsner, facilmente encontradas nas representantes Bitburger, Warsteiner, Konig Pilsener, Spaten Pils.",
   },
   {
     name: "Cerveja Lager",
     value: 12.56,
     image: beer,
+    description:
+      "Cerveja leve e refrescante, feita para matar a sede e para serem bebidas bem geladas. É o tipo das cervejas mais populares dos Estados Unidos, com exemplos como Budweiser, Coors e a australiana Foster's. A maioria das cervejas populares no Brasil, como Brahma, Skol, Kaiser e Antarctica, são American Lagers, mesmo que elas se intitulem no rótulo e descrição no verso como Pilsen, talvez até devido a convenção nacional para a classificação de cervejas.",
   },
   {
     name: "Cerveja Pale Ale",
     value: 12.56,
     image: beer,
+    description:
+      "A Pale Lager original, desenvolvida como receita da cerveja Pilsner Urquell. São caracterizadas por um lúpulo acentuado no aroma e sabor. Também chamadas de Pilsener e Pilsen. Podem aparecer em 2 estilos um pouco diferentes, principalmente devido à escola: Bohemian/Czech Pilsner , representada pela Pilsner Urquell e Budweiser Budvar/Czechvar, ambas Tchecas e meio difíceis de encontrar; German Pilsner, facilmente encontradas nas representantes Bitburger, Warsteiner, Konig Pilsener, Spaten Pils.",
   },
   {
     name: "Cerveja Bock",
     value: 12.56,
     image: beer,
+    description:
+      "A Pale Lager original, desenvolvida como receita da cerveja Pilsner Urquell. São caracterizadas por um lúpulo acentuado no aroma e sabor. Também chamadas de Pilsener e Pilsen. Podem aparecer em 2 estilos um pouco diferentes, principalmente devido à escola: Bohemian/Czech Pilsner , representada pela Pilsner Urquell e Budweiser Budvar/Czechvar, ambas Tchecas e meio difíceis de encontrar; German Pilsner, facilmente encontradas nas representantes Bitburger, Warsteiner, Konig Pilsener, Spaten Pils.",
   },
   {
     name: "Cerveja Weissbier",
     value: 12.56,
     image: beer,
+    description:
+      "A Pale Lager original, desenvolvida como receita da cerveja Pilsner Urquell. São caracterizadas por um lúpulo acentuado no aroma e sabor. Também chamadas de Pilsener e Pilsen. Podem aparecer em 2 estilos um pouco diferentes, principalmente devido à escola: Bohemian/Czech Pilsner , representada pela Pilsner Urquell e Budweiser Budvar/Czechvar, ambas Tchecas e meio difíceis de encontrar; German Pilsner, facilmente encontradas nas representantes Bitburger, Warsteiner, Konig Pilsener, Spaten Pils.",
   },
   {
     name: "Cerveja Stout",
     value: 12.56,
     image: beer,
+    description:
+      "A Pale Lager original, desenvolvida como receita da cerveja Pilsner Urquell. São caracterizadas por um lúpulo acentuado no aroma e sabor. Também chamadas de Pilsener e Pilsen. Podem aparecer em 2 estilos um pouco diferentes, principalmente devido à escola: Bohemian/Czech Pilsner , representada pela Pilsner Urquell e Budweiser Budvar/Czechvar, ambas Tchecas e meio difíceis de encontrar; German Pilsner, facilmente encontradas nas representantes Bitburger, Warsteiner, Konig Pilsener, Spaten Pils.",
   },
   {
     name: "Cerveja Porter",
     value: 12.56,
     image: beer,
+    description:
+      "A Pale Lager original, desenvolvida como receita da cerveja Pilsner Urquell. São caracterizadas por um lúpulo acentuado no aroma e sabor. Também chamadas de Pilsener e Pilsen. Podem aparecer em 2 estilos um pouco diferentes, principalmente devido à escola: Bohemian/Czech Pilsner , representada pela Pilsner Urquell e Budweiser Budvar/Czechvar, ambas Tchecas e meio difíceis de encontrar; German Pilsner, facilmente encontradas nas representantes Bitburger, Warsteiner, Konig Pilsener, Spaten Pils.",
   },
 ];
 
@@ -66,8 +84,14 @@ function App() {
       <div className="items--container">
         <div className="main-content--container">
           <div className="main-content">
-            {content.map((item) => (
-              <Item image={item.image} name={item.name} value={item.value} />
+            {content.map((item, key) => (
+              <Item
+                key={key}
+                image={item.image}
+                name={item.name}
+                value={item.value}
+                description={item.description}
+              />
             ))}
           </div>
         </div>
--- src/ui/components/Item.component.tsx
+++ src/ui/components/Item.component.tsx
@@ -2,15 +2,37 @@ import React from "react";
 
 import "./Item.component.css";
 
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+
+const MySwal = withReactContent(Swal);
+
 interface ItemComponentInterface {
   image: string;
   name: string;
   value: number;
+  description?: string;
 }
 
 const Item = (data: ItemComponentInterface) => {
+  const modalContent = (
+    <>
+      <img className="modal--image" src={data.image} alt="Teste" />
+      <p className="modal--content">{data.description}</p>
+    </>
+  );
+  const showModal = () => {
+    MySwal.fire({
+      title: <p>{data.name}</p>,
+      html: modalContent,
+      didOpen: () => {},
+    }).then(() => {});
+
+    return undefined;
+  };
+
   return (
-    <div className="item--container">
+    <div className="item--container" onClick={showModal}>
       <div className="item--image">
         <img src={data.image} alt="Teste" />
       </div>
